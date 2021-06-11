e631d85243c08b1fc0de3868dd2ba26182775f53
joelstevick <joelstevick@outlook.com>
2021-06-11 15:26:29 -0400

    1 file modified

--- src/app/model/model.ts
+++ src/app/model/model.ts
@@ -2,29 +2,51 @@ import { Vertice } from './vertice.interface';
 
 export const tree: Vertice = {
   label: 'Walt Disney $550',
-  color: 'green',
+  color: '#7d5a50',
   children: [
     {
       label: 'Movies $250',
-      color: 'blue',
+      color: '#e5b299',
       children: [
         {
-          label: 'Snow White $50'
+          label: 'Snow White $50',
+          color: '#fcdec0'
         },
         {
-          label: 'Bambi $200'
+          label: 'Bambi $200',
+          color: '#fcdec0'
         }
       ]
     },
     {
       label: 'Theme Parks $300',
-      color: 'orange',
+      color: '#e5b299',
       children: [
         {
-          label: 'DisneyWorld $200'
+          label: 'DisneyWorld $200',
+          children: [
+            {
+              label: 'Rides $150',
+              color: '#fcdec0'
+            },
+            {
+              label: 'Concessions $50',
+              color: '#fcdec0'
+            }
+          ]
         },
         {
-          label: 'DisneyLand $100'
+          label: 'DisneyLand $100',
+          children: [
+            {
+              label: 'Rides $75',
+              color: '#fcdec0'
+            },
+            {
+              label: 'Concessions $25',
+              color: '#fcdec0'
+            }
+          ]
         }
       ]
     }
