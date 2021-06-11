b9006101e031989a2403229c5dbe137f57c238cd
joelstevick <joelstevick@outlook.com>
2021-06-11 10:01:50 -0400

    1 file modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavigatorComponent } from './components/navigator/navigator.component';
+import { PathComponent } from './components/navigator/path/path.component';
+import { ChildrenComponent } from './components/navigator/children/children.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, NavigatorComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    NavigatorComponent,
+    PathComponent,
+    ChildrenComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
