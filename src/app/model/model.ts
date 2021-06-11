a743b9038adcfbd27488f7c4101dd91172520c5f
joelstevick <joelstevick@outlook.com>
2021-06-11 13:41:07 -0400

    1 file modified

--- src/app/model/model.ts
+++ src/app/model/model.ts
@@ -1,6 +1,36 @@
 import { Vertice } from './vertice.interface';
 
 export const tree: Vertice = {
+  label: 'Walt Disney',
+  color: 'green',
+  children: [
+    {
+      label: 'Movies',
+      color: 'blue',
+      children: [
+        {
+          label: 'Snow White $50'
+        },
+        {
+          label: 'Bambi $200'
+        }
+      ]
+    },
+    {
+      label: 'Theme Parks',
+      color: 'orange',
+      children: [
+        {
+          label: 'DisneyWorld $200'
+        },
+        {
+          label: 'DisneyLand $100'
+        }
+      ]
+    }
+  ]
+};
+export const tree2: Vertice = {
   label: 'NFL',
   color: 'green',
   children: [
